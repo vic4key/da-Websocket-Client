8a92bbe664cde69256ccbfd961ca05ba6ccd3500
Vic P <vic4key@gmail.com>
2022-09-12 21:51:49 +0700

    Initial

--- plugin.py
+++ plugin.py
@@ -4,28 +4,28 @@ from pluginlib import Parent
 
 @Parent("Plugin")
 class PluginTemplate(object):
-    ''' Plugin Template '''
+  ''' Plugin Template '''
 
-    def on_open(self, ws): 
-        return
+  def on_open(self, ws): 
+    return
 
-    def on_close(self, ws, close_status_code, close_msg): 
-        return
+  def on_close(self, ws, close_status_code, close_msg): 
+    return
 
-    def on_error(self, ws, error): 
-        return
+  def on_error(self, ws, error): 
+    return
 
-    def on_recv(self, ws, data, type, continuous): 
-        return
+  def on_recv(self, ws, data, type, continuous): 
+    return
 
-    def on_send(self, ws, data, opcode): 
-        return
+  def on_send(self, ws, data, opcode): 
+    return
 
-    def on_ping(self, ws, message): 
-        return
+  def on_ping(self, ws, message): 
+    return
 
-    def on_pong(self, ws, message): 
-        return
+  def on_pong(self, ws, message): 
+    return
 
 # Plugin Manager
 
@@ -34,14 +34,14 @@ from pluginlib import PluginLoader, PluginImportError
 _plugins = None
 
 def initialize():
-    global _plugins
-    if _plugins is None:
-        loader = PluginLoader(modules=["plugins"])
-        try:
-            _plugins = loader.plugins
-        except PluginImportError as e:
-            print(e)
+  global _plugins
+  if _plugins is None:
+    loader = PluginLoader(modules=["plugins"])
+    try:
+      _plugins = loader.plugins
+    except PluginImportError as e:
+      print(e)
 
 def plugins():
-    global _plugins
-    return None if _plugins is None else _plugins.Plugin.values()
+  global _plugins
+  return None if _plugins is None else _plugins.Plugin.values()
--- test/websocket_server.py
+++ test/websocket_server.py
@@ -8,25 +8,25 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 async def handler(ws, path):
-    try:
-        while True:
-            data = await ws.recv()
-            print(f"WS Server ({ws.path}) sent '{data}'")
-            await ws.send(data)
-    except websockets.exceptions.ConnectionClosedOK as e:
-        print("Client closed", e)
-    except Exception as e:
-        print("Exception:", e)
+  try:
+    while True:
+      data = await ws.recv()
+      print(f"WS Server ({ws.path}) sent '{data}'")
+      await ws.send(data)
+  except websockets.exceptions.ConnectionClosedOK as e:
+    print("Client closed", e)
+  except Exception as e:
+    print("Exception:", e)
 
 if __name__ == "__main__":
-    # create ssl context for websocket secure
-    ssl_context = None
-    if len(sys.argv) == 2 and sys.argv[1].lower() == "secure":
-        normalize_path = lambda path: path.replace("\\", "/").replace("/", os.path.sep)
-        ssl_file_path = normalize_path(os.path.join(pathlib.Path(__file__).resolve().parent.parent, "preferences/local.pem"))
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        ssl_context.load_cert_chain(ssl_file_path)
-    # start websocket server
-    ws_server = websockets.serve(handler, "127.0.0.1", 1609, ssl=ssl_context)
-    asyncio.get_event_loop().run_until_complete(ws_server)
-    asyncio.get_event_loop().run_forever()
+  # create ssl context for websocket secure
+  ssl_context = None
+  if len(sys.argv) == 2 and sys.argv[1].lower() == "secure":
+    normalize_path = lambda path: path.replace("\\", "/").replace("/", os.path.sep)
+    ssl_file_path = normalize_path(os.path.join(pathlib.Path(__file__).resolve().parent.parent, "preferences/local.pem"))
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(ssl_file_path)
+  # start websocket server
+  ws_server = websockets.serve(handler, "127.0.0.1", 1609, ssl=ssl_context)
+  asyncio.get_event_loop().run_until_complete(ws_server)
+  asyncio.get_event_loop().run_forever()
